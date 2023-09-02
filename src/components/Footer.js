657a3ab6455a263ae6c8bfd105b53357272fb9d6
jakChi <kobachincharauli8@gmail.com>
2023-09-02 19:20:27 +0400

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,51 @@ import React from "react";
 import "./Footer.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngry } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faXTwitter,
+  faLinkedinIn,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
-const FACEBOOK = <FontAwesomeIcon icon={faAngry} />
+const FACEBOOK = <FontAwesomeIcon icon={faFacebookF} />;
+const X = <FontAwesomeIcon icon={faXTwitter} />;
+const LINKEDIN = <FontAwesomeIcon icon={faLinkedinIn} />;
+const GIT = <FontAwesomeIcon icon={faGithub} />
 
 const Footer = () => {
   return (
     <div className="footer">
       <ul className="soc-links">
-        <Link><i>{FACEBOOK}</i></Link>
-        <Link>Tweeter</Link>
-        <Link>Linkedin</Link>
+        <Link>
+          <i>{FACEBOOK}</i>
+        </Link>
+        <Link>
+          <i>{X}</i>
+        </Link>
+        <Link>
+          <i>{LINKEDIN}</i>
+        </Link>
+        <Link>
+          <i>{GIT}</i>
+        </Link>
       </ul>
-      <p>&copy;2021 Nadine Coelho | All Rights Reserved</p>
+      <ul className="other-links">
+        <Link>Create React App</Link>
+        <Link>React Router</Link>
+        <Link>some other referance</Link>
+      </ul>
+      <address className="copyright">
+        <p>&copy; Developed by jakChi 2023 | All Rights Reserved</p>
+      </address>
     </div>
   );
 };
 
 export default Footer;
+
+<div class="parent">
+  <div class="div1"> </div>
+  <div class="div2"> </div>
+  <div class="div3"> </div>
+</div>;
