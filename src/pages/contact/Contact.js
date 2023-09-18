8ad4658ae549eb5483400c564b829089a5e321e2
jakChi <kobachincharauli8@gmail.com>
2023-09-18 23:52:49 +0400

    chnaged Hero and contact files

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -3,12 +3,12 @@ import "./Contact.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
 //import { Link } from "react-router-dom";
-import resume from "../../assets/files/resume2.docx.pdf";
+//import resume from "../../assets/files/resume2.docx.pdf";
 
 const Contact = () => {
   const [input, setInput] = useState("");
   const [email, setEmail] = useState("");
-  const [pdfOpen, setPdfOpen] = useState(false);
+  // const [pdfOpen, setPdfOpen] = useState(false);
 
   const actionUrlAgent = `mailto:kobachincharauli8@gmail.com?subject=New customer from portfolio&body=Hi, Contact me on this email: ${email}!`;
   const actionUrlGmail = `https://mail.google.com/mail/?view=cm&fs=1&to=kobachincharauli5@gmail.com&su=New+Customer+from+portfolio&body=Hi,+contact+me+on+this+email:+${email}`;
@@ -39,15 +39,17 @@ const Contact = () => {
           method="post"
           onSubmit={handleSubmit}
         >
-          <h1 className="title">submit your email & I'll contact</h1>
-          <input
-            req="true"
-            className="email-input"
-            type="email"
-            placeholder="input your email"
-            value={input}
-            onChange={handleChange}
-          />
+          <label className="email-label">
+            Sumbit your email and I will contact
+            <input
+              req="true"
+              className="email-input"
+              type="email"
+              placeholder="input your email"
+              value={input}
+              onChange={handleChange}
+            />
+          </label>
           <p className={`sub-text ${input ? "active" : null}`}>
             press enter key to submit your email <i>{bulb}</i>
           </p>
@@ -55,14 +57,31 @@ const Contact = () => {
           <input className={`sub-button ${input ? "active" : null}`} type="submit" onClick={handleSubmit} />*/}
         </form>
       </section>
-      <section className="about">
-        <h1 className="title">ABOUT ME</h1>
-        <button className="resume-btn" onClick={() => setPdfOpen(!pdfOpen)}>
+      <section className="info">
+        <h1 className="title">About me</h1>
+        <ul className="about-list">
+          <li>I am a self taught React developer</li>
+          <li>Currently my study field is Computational biology</li>
+          <li>I can provide you with my full attention</li>
+          <li>I will always be in touch with you and you can contact me any time</li>
+        </ul>
+        <h1 className="title">What I offer</h1>
+        <ul className="product-list">
+          <li>Development of your SPA's frontend</li>
+          <li>Written with newest technologies</li>
+          <li>React functional components & hooks</li>
+          <li>React class based components</li>
+          <li>ES6 javascript</li>
+          <li>Fully Responsive Design with CSS3</li>
+          <li>3 months free bug fix</li>
+          <li>1 months free changes for new customers</li>
+        </ul>
+        {/* <button className="resume-btn" onClick={() => setPdfOpen(!pdfOpen)}>
           {pdfOpen ? "close" : "open"} resume
         </button>
         {pdfOpen ? (
           <embed src={resume} type="application/pdf" className="resume" />
-        ) : null}
+        ) : null} */}
       </section>
     </div>
   );
--- src/pages/home/components/Hero.js
+++ src/pages/home/components/Hero.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../Home.css";
 import video from "../../../assets/videos/gradient.mp4";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 //import { faReact } from "@fortawesome/free-brands-svg-icons";
 
 //document.getElementsByClassName("video").defaultPlaybackRate = 3.0;
@@ -20,24 +21,22 @@ const Hero = ({ mobile }) => {
           poster="https://th.bing.com/th/id/OIP.gYyvBOuQwXSWuefMRBZp9gAAAA?pid=ImgDet&rs=1"
         />
 
-				{/*
+        {/*
 				<i><FontAwesomeIcon icon={faReact}/></i>
-				<span className="gradient-word">React</span>
-				*/}
 				
-        <h1 className="welcome-txt">Welcome to my portfolio!</h1>
+				*/}
+
+        <h1 className="welcome-txt">
+          <span className="gradient-word">Welcome</span> to my portfolio!
+        </h1>
         <div className="txt-btn-group">
           <div className="contact-div">
-            <p className="contact-text">
-							everything you need to know about me
-            </p>
-            <button className="pixel-border">Click me!</button>
+            <p className="contact-text">everything you need to know about me</p>
+            <Link to={"contact"} className="pixelated-btn">Click me!</Link>
           </div>
           <div className="projects-div">
-            <p className="projects-text">
-              visit my projects page here
-            </p>
-						<button className="pixel-border">Click me!</button>
+            <p className="projects-text">visit my projects page here</p>
+            <Link to={"projects"} className="pixelated-btn">Click me!</Link>
           </div>
         </div>
       </div>
