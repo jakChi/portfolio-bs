ad33258db1f4525305e4382363a55b8fa9aa8304
jakChi <kobachincharauli8@gmail.com>
2024-04-09 20:25:01 +0400

    minor update

--- src/pages/projects/ProjectsData.js
+++ src/pages/projects/ProjectsData.js
@@ -3,13 +3,15 @@ import shopList from "../../assets/images/shopping-list.png";
 import ticTacToe from "../../assets/images/tictactoe.png";
 import RandomQuote from "../../assets/images/randomQuote.png";
 import bmiCalc from "../../assets/images/BMI.png";
+import pomodoro from "../../assets/images/pomodoro.png";
+import jlog from "../../assets/images/jlog.png";
 
 const ProjectsArr = [
   {
     id: 1,
     name: "Retro Calculator",
     githubUrl: "https://github.com/jakChi/retro-calculator",
-    snippet: `https://codepen.io/CamperIO/embed/GRGmQxm?default-tab=result&editable=true&theme-id=dark`,
+    snippet: `https://codepen.io/CamperIO/embed/preview/GRGmQxm?default-tab=result&editable=true&theme-id=dark`,
     className: "calc",
     picSrc: calc,
     caption: "Retro Style React Calculator",
@@ -18,7 +20,7 @@ const ProjectsArr = [
     id: 2,
     name: "Shopping List",
     githubUrl: "https://github.com/jakChi/Shopping-list",
-    snippet: `https://codepen.io/CamperIO/embed/qBQvPVL?default-tab=result&editable=true&theme-id=dark`,
+    snippet: `https://codepen.io/CamperIO/embed/preview/qBQvPVL?default-tab=result&editable=true&theme-id=dark`,
     className: "shop-list",
     picSrc: shopList,
     caption: "Simple shopping list",
@@ -27,7 +29,7 @@ const ProjectsArr = [
     id: 3,
     name: "Tic-Tac-Toe",
     githubUrl: "https://github.com/jakChi/Tic-Tac-Toe",
-    snippet: `https://codepen.io/CamperIO/embed/oNaJVbO?default-tab=result&editable=true&theme-id=dark`,
+    snippet: `https://codepen.io/CamperIO/embed/preview/oNaJVbO?default-tab=result&editable=true&theme-id=dark`,
     className: "tictactoe",
     picSrc: ticTacToe,
     caption: "Tic-Tac-Toe game",
@@ -36,7 +38,7 @@ const ProjectsArr = [
     id: 4,
     name: "Random Quotes",
     githubUrl: "",
-    snippet: `https://codepen.io/CamperIO/embed/ExLaPgZ?default-tab=result&editable=true&theme-id=dark`,
+    snippet: `https://codepen.io/CamperIO/embed/preview/ExLaPgZ?default-tab=result&editable=true&theme-id=dark`,
     className: "random-qoute",
     picSrc: RandomQuote,
     caption: "Random Quote Generator",
@@ -45,10 +47,28 @@ const ProjectsArr = [
     id: 5,
     name: "BMI Calculator",
     githubUrl: "",
-    snippet: `https://codepen.io/CamperIO/embed/YzRMVBo?default-tab=result&editable=true&theme-id=dark`,
+    snippet: `https://codepen.io/CamperIO/embed/preview/YzRMVBo?default-tab=result&editable=true&theme-id=dark`,
     className: "bmiCalc",
     picSrc: bmiCalc,
     caption: "Body Mass Index calculator",
+  },
+  {
+    id: 6,
+    name: "Pomodoro Timer",
+    githubUrl: "https://github.com/jakChi/Pomodoro-timer",
+    snippet: `https://codepen.io/CamperIO/embed/preview/yLwJYRg?default-tab=result&editable=true&theme-id=dark`,
+    className: "pomodoro",
+    picSrc: pomodoro,
+    caption: "Pomodoro Timer",
+  },
+  {
+    id: 7,
+    name: "Jlog",
+    githubUrl: "https://github.com/jakChi/Jlog",
+    snippet: ``,
+    className: "jlog",
+    picSrc: jlog,
+    caption: "Blogging Website",
   },
 ];
 
